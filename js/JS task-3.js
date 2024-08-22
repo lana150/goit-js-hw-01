714655585f79fc5bd9fca84fc8443ce40149d6dc
Lana150 <astra.altair150@gmail.com>
2024-08-23 01:41:42 +0200

    added changes

--- js/JS task-3.js
+++ js/JS task-3.js
@@ -9,6 +9,6 @@ function getElementWidth(content, padding, border) {
     return totalWidth;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getElementWidth("50px", "8px", "4px")) 
+console.log(getElementWidth("60px", "12px", "8.5px"))
+console.log(getElementWidth("200px", "0px", "0px")) 
